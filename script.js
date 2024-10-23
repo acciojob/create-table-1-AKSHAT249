c5670d1798adbaac72a103addf631b047c405f46
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-10-23 17:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
+
+	let table = document.getElementById("sampleTable");
+	let new_row = `
+		<tr>
+			<td>New Cell1</td>
+			<td>New Cell 2</td>
+		</tr>
+	`;
+
+	table.innerHTML = new_row + table.innerHTML;
   
 }
